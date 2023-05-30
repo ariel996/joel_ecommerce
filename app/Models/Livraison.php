6e88226a3c27318767e200ea48895a2ef5f418f7
Ariel De Guilique NANA YANDJEU <arieldeguilique@hotmail.com>
2023-05-30 11:52:21 +0100

    Update Livraison.php

--- app/Models/Livraison.php
+++ app/Models/Livraison.php
@@ -16,4 +16,9 @@ class Livraison extends Model
     {
         return $this->belongsTo(Order::class);
     }
+    
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+        }
 }
